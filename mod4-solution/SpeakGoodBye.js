39f5879d36628c456e033e9e419225fd7c697247
Akempe2020 <91756877+Akempe2020@users.noreply.github.com>
2021-12-22 14:40:29 -0500

    Update SpeakGoodBye.js

--- mod4-solution/SpeakGoodBye.js
+++ mod4-solution/SpeakGoodBye.js
@@ -18,7 +18,7 @@
 // See Lecture 52, part 2
 (function speak(name) {
   var byeSpeaker = {}
-  byeSpeaker.name = names
+  byeSpeaker.name = "Yaakov";
   var speakWord = "Good Bye";
   
   console.log(speakWord + " " + name);
